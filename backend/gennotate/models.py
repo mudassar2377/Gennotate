11674552874e4c8e6da8eeced00ab68c71d0032a
Hashir789 <m.hashir.malik1020@gmail.com>
2024-02-22 12:28:48 +0500

    Commit 2 - Setting up basic models template

---
+++ backend/gennotate/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
+    def __str__(self):
+        return f"User # {self.id}: {self.username}"
